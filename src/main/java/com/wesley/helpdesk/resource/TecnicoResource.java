59cc87f28551485b886b370371d1af5c9a0789f3
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-05-27 18:44:28 -0300

    Endpoint findById para tecnicos

---
+++ src/main/java/com/wesley/helpdesk/resource/TecnicoResource.java
@@ -0,0 +1,28 @@
+package com.wesley.helpdesk.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesley.helpdesk.domain.Tecnico;
+import com.wesley.helpdesk.services.TecnicoService;
+
+@RestController //Definindo que a classe será um controlador rest para realizar requisições
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	private TecnicoService service;
+	
+	@GetMapping(value = "/{id}")//Informando que estou recebendo uma variável de path
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) { //Response entity significa que queremos representar toda a resposta http, sendo possível controlar qualquer coisa envolvendo a requisição
+		
+		Tecnico obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/wesley/helpdesk/services/TecnicoService.java
@@ -0,0 +1,23 @@
+package com.wesley.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesley.helpdesk.domain.Tecnico;
+import com.wesley.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		//Optional define que o id pode ou não ser encontrado
+		Optional<Tecnico> obj = repository.findById(id);
+		
+		return obj.orElse(null);
+	}
+}
