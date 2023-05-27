539a1f35c1949e5c129f29493e3651b57a42e755
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-05-27 18:04:24 -0300

    Criando perfil de desenvolvimento

---
+++ src/main/java/com/wesley/helpdesk/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.wesley.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.wesley.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbservice;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbservice.instanciaDB();
+		}
+		return false;
+	}
+}
