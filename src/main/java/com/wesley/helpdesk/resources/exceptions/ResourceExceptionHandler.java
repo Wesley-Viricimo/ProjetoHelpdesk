dcfa80b1948db3ce7306e2ec39e494a0aaf52be9
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-05-28 22:56:05 -0300

    Exceção para CPF e Email já cadastrados

--- src/main/java/com/wesley/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/wesley/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.wesley.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.wesley.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Object Not Found", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)//definindo que o método é um manipulador de excessões da classe DataIntegrityViolationException
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
--- src/main/java/com/wesley/helpdesk/services/TecnicoService.java
+++ src/main/java/com/wesley/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wesley.helpdesk.domain.Pessoa;
 import com.wesley.helpdesk.domain.Tecnico;
 import com.wesley.helpdesk.domain.dtos.TecnicoDTO;
+import com.wesley.helpdesk.repositories.PessoaRepository;
 import com.wesley.helpdesk.repositories.TecnicoRepository;
+import com.wesley.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.wesley.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +19,8 @@ public class TecnicoService {
 	
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoarepository;
 	
 	public Tecnico findById(Integer id) {
 		//Optional define que o id pode ou não ser encontrado
@@ -30,7 +35,22 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoarepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema: " + objDTO.getCpf());
+		}
+		
+		obj = pessoarepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema: " + objDTO.getEmail());
+		}
+		
+		
 	}
 }
