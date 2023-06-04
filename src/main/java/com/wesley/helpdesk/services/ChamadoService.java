8789a23d6e4c93c6e6835f2ea992272574aae1be
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-06-04 16:39:36 -0300

    FindById para chamados

---
+++ src/main/java/com/wesley/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.wesley.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesley.helpdesk.domain.Chamado;
+import com.wesley.helpdesk.repositories.ChamadoRepository;
+import com.wesley.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id); //Optional define que o objeto pode ou não ser encontrado
+		return obj.orElseThrow(() -> new ObjectNotFoundException("O chamado '" + id +  "' não foi encontrado!")); //Se o objeto não for encontrado será disparada a exceção customizada
+	}
+}
